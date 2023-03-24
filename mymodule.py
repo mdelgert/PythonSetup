f86e2fbba8ccb485c24d0be11261a730fe9353c5
Matthew David Elgert <mdelgert@yahoo.com>
2023-03-23 22:56:39 -0400

    add mymodule.py

---
+++ mymodule.py
@@ -0,0 +1,4 @@
+#https://www.w3schools.com/python/python_modules.asp
+
+def greeting(name):
+  print("Hello, " + name)
---
+++ mymoduleImport.py
@@ -0,0 +1,5 @@
+#https://www.w3schools.com/python/python_modules.asp
+
+import mymodule
+
+mymodule.greeting("Jonathan")
